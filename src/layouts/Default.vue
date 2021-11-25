c91085de679c88c1e1ee567a846eba75625bd77e
Guiomar Valderrama <guival4@gmail.com>
2021-11-25 19:22:02 +0100

    remove vuetify deprecated flat

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -10,7 +10,7 @@
         </g-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat to="/pokemon/">
+      <v-btn to="/pokemon/">
         <span class="mr-2">Get Pokemon</span>
       </v-btn>
     </v-app-bar>
@@ -20,17 +20,17 @@
     </v-container>
 
     <v-footer dark padless>
-      <v-card flat tile width="100%" class="blue-grey darken-4 white--text text-center">
+      <v-card tile width="100%" class="blue-grey darken-4 white--text text-center">
         <v-card-text>
           <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon :href="icon.url" target="_blank">
             <v-icon>{{ icon.image }}</v-icon>
           </v-btn>
         </v-card-text>
-  
+
         <v-card-text class="white--text pt-0">
           This is a Vue project that uses Gridsome and Vuetify.
         </v-card-text>
-  
+
       </v-card>
     </v-footer>
 
